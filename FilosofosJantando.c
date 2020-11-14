6ee999c19b4e6a279889e53287a8df372e1b7fcf
Heliks <heliksmm@hotmail.com>
2020-11-14 03:20:17 -0300

    criando função comer base

--- FilosofosJantando.c
+++ FilosofosJantando.c
@@ -3,6 +3,10 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <time.h>
+
+#define FECHADO 0       //Id para estado semaforo
+#define ABERTO 1        //Id para estado semaforo
+
   
 #define PENSAR 0        //Id para estado pensando
 #define ESPERAR 1       //Id para estado esperando
@@ -12,24 +16,24 @@ typedef struct nfilosofos{      //Estrutura de dados dos filósofos
         int quantidadeF;              //Quantidade de filósofos
         int quantidadeM;			//Quantidade de macarrão
         int id;                 //Índice do filósofo
-        int tempoPensar;		// Tempo de cada filosofo para pensar
       	int estado;				// estado em que se encontra o filosofo
       	
 }NFilosofos;
 
+int qMacarrao;
 sem_t macarrao;        //Representa a comida que os filosofos irão comer
 sem_t *garfo;           //Vetor que representa os garfos
  
  	void *filosofo (void *F);
-//	void comer (NFilosofos F);
+	void comer (void *F);
 //	void esperar (NFilosofos F);
-	void pensar (int id);
+	void pensar (void *F);
 //	void mostrar (int i);
 //	void teste (int i,int quantidade);
 
 
 void main (){
-	int qFilo, qMacarrao, i;
+	int qFilo, i;
 	NFilosofos *vetorFilo; //vetor de structs Nfilosofos
 	pthread_t *thFilo; //vetor de threads dos filosofos
 	
@@ -53,7 +57,7 @@ void main (){
         thFilo = (pthread_t*) malloc ((qFilo)*sizeof(pthread_t));   //Aloca vetor de threads
         garfo = (sem_t*)malloc((qFilo)*sizeof(sem_t));    //Aloca a quantidade de garfos = quantidade de filósofos
 
-        sem_init(&macarrao, 0, qMacarrao); //semaforo do macarrao
+        sem_init(&macarrao, 0, ABERTO); //semaforo do macarrao
         
         for (i=0; i<qFilo; i++){
         	 sem_init(&garfo[i], 0, 2); //semaforo dos garfos
@@ -61,7 +65,6 @@ void main (){
 
         for (i=0;i<qFilo;i++){         //Inicializa o vetor com os dados dos filósofos
             vetorFilo[i].quantidadeF = qFilo;
-            vetorFilo[i].tempoPensar = rand() % 1000;  //tempo aleatório para pensar
       
        	    vetorFilo[i].id = i;		//indice do filosofo
             vetorFilo[i].estado = PENSAR;         //cada filosofo inicia no estado pensar
@@ -91,21 +94,44 @@ void *filosofo(void *F){
 	 NFilosofos Filo = *(NFilosofos*) F;
        
         while (1){
-                pensar(Filo.id);     
+                pensar(F); 
+                comer(F);    
         }
 }
 
 
 //cria o pensamento do filosofo
-void pensar(int id){
+void pensar(void *F){
+    NFilosofos *Filo = (NFilosofos*) F;
 
     int tempo;
-    tempo=(rand() % 5+1);
+    tempo=(rand() % 5+1);           //tempo para pensar
+
+    printf("\nfilosofo %d a pensar por %ds", Filo->id,tempo);
+
     usleep(tempo*1000000);          //deixa o filosofo pensando por alguns milissegundos
-         
-    printf("\nfilosofo %d a pensar por %ds", id,tempo);
+    Filo->quantidadeM = tempo; //quantidade de macarrao que irá comer na proxima vez
 
+    
 
+}
+
+void comer(void *F){
+    NFilosofos *Filo = (NFilosofos*) F;
+
+    sem_wait(&(macarrao));      //bloqueia o semaforo do macarrao
+
+    if(qMacarrao <=0){          //verifica se ainda há macarrão
+        printf("\nFilosofo %d foi tentar comer %d, mas Macarrao acabou",Filo->id,Filo->quantidadeM);
+        exit(1);
+    }
+    qMacarrao = qMacarrao- Filo->quantidadeM;   // decrementa do macarrão
+    if(qMacarrao<=0){           //para não ter macarrão negativo
+        qMacarrao=0;
+    }
+    printf("\nFilosofo %d comeu %d Macarrao total %d",Filo->id,Filo->quantidadeM,qMacarrao);
+
+    sem_post(&(macarrao));      //desbloqueia o semaforo 
 }
 
 
